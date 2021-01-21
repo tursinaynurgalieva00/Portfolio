1a72a5e48732ca38c14767933c2d4b443d4276f9
tursinaynurgalieva00 <nurgalievatursinay@mail.ru>
2021-01-22 00:01:45 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="navBar">
+      <ul>
+        <li>
+          <a href="">Online shop</a>
+        </li>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">City</a>
+        </li>
+        <li>
+          <a href="">Contacts</a>
+        </li>
+      </ul>
+    </div>
+    </div>
+  );
+}
+
+export default App;
